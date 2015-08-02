2f39df021ea61edd40fc9f5c950e938d1a566aed
Roy Yang <roycyang@gmail.com>
2015-08-02 11:51:23 -0400

    adding new logo

--- js/custom.js
+++ js/custom.js
@@ -12,8 +12,11 @@
 	$(window).scroll(function() {
 		if ($(".navbar").offset().top > 50) {
 			$(".navbar-fixed-top").addClass("top-nav-collapse");
+      $(".navbar-fixed-top").find('img').attr('src', 'img/logo.png');
 		} else {
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
+      $(".navbar-fixed-top").find('img').attr('src', 'img/giant_machines_logo.png');
+
 		}
 	});
 
