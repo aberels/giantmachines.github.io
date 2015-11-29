433625b6dae666c4c869ebc53b0f0ead51e84c23
agoldvarg <agoldvarg@gmail.com>
2015-11-29 18:04:25 -0500

    Tweak responsive styles

--- js/main.js
+++ js/main.js
@@ -3,35 +3,45 @@ $(function() {
   var panelHeight = $('.panel').height();
   var activePanel = 'panel-primary';
   var awatingInteraction = true;
+  var navHeight = $('.nav-header').height();
+  var footerHeight = $('.footer').height();
 
+  // If Desktop, set navHeight to 0
+  if (navHeight > 100) { navHeight = 0 };
+
+  // Auto-scroll
+  // autoScrollCarousel()
+
+  /** SetTimeout for scrolling the main content automatically */
+  function autoScrollCarousel() {
+    var scrollDistance = panelHeight;
+    setInterval(function(){
+      if (awatingInteraction) {
+        if (scrollDistance === (panelHeight * 4)) { scrollDistance = 0 }
+        scrollCarousel(scrollDistance);
+        scrollDistance += panelHeight;
+      }
+    }, 4000);
+  }
+
+  /**
+   * Sets the active class on a panel
+   * @param {object} panel - The jQuery panel el
+   */
   function setActivePanel(panel) {
     $('.link').not(panel).removeClass('active');
     $(panel).addClass('active');
   }
 
-  // Autoscrolling when users has not yet clicked
-  function autoScrollCarousel() {
-    var scrollDistance = panelHeight;
-    setInterval(function(){
-      if (awatingInteraction) {
-        scrollDistance > $('body').height() ? scrollDistance = 0 : true
-        scrollCarousel(scrollDistance);
-        scrollDistance += panelHeight;
-      }
-    }, 1000);
-  }
-
-  // Scrolls window to active panel
+  /**
+   * Scrolls the page
+   * @param {integer} offset - pixes to scroll
+   */
   function scrollCarousel(offset) {
-    $('body').animate({
-      scrollTop: offset
-    }, 500)
+    $('body').animate({scrollTop: offset}, 1000, 'easeInOutQuart');
   }
 
-  // Trigger autoscroll
-  // autoScrollCarousel()
-
-  // While scrolling, set the active panel
+  // Sets the active panel on page scroll
   $(window).scroll(function() {
     var windowPosition = $(this).scrollTop();
 
@@ -39,19 +49,24 @@ $(function() {
       var linkedPanelClass = $(this).data('panel');
       var linkedPanelOffset = $('.' + linkedPanelClass).offset().top;
 
-      if (windowPosition >= linkedPanelOffset) {
+      if (windowPosition >= linkedPanelOffset - navHeight) {
         setActivePanel(this);
       }
     });
   });
 
-  // Click handler for carousel
+  // Carousel Click Handler
   $('.link').click(function() {
     awatingInteraction = false;
     var linkedPanelClass = $(this).data('panel');
     var linkedPanelOffset = $('.' + linkedPanelClass).offset().top;
 
-    scrollCarousel(linkedPanelOffset);
+    scrollCarousel(linkedPanelOffset - navHeight);
+  });
+
+  // Menu Click Handler
+  $('.nav-control').click(function() {
+    $('.nav-collapse').slideToggle(300);
   });
 
 });
